09cf776ec8948371749093f7720d61ab68b18dfc
Gabriel Lencioni Lovate <gabriel_lovate@hotmail.com>
2021-04-25 22:16:55 +0200

    Clean the code

--- variance_by_element.py
+++ variance_by_element.py
@@ -16,31 +16,37 @@ def plot_heatmap(array, output_dir):
 def plot_histogram(array, output_dir):
     histogram = plt.figure()
     ax = histogram.add_subplot(111)
-    ax.hist(array.flatten(), bins='auto')
+    ax.hist(array.flatten(), bins="auto")
     histogram.tight_layout()
     histogram.savefig(output_dir)
     plt.close(histogram)
 
+
 def regex_from_segments(viral_segments):
     """
-    Returns a compiled regex that can be used to search for pairs of viral segment 
-    abreviations in a given string.
+    Returns a compiled regex that can be used to search for pairs of
+    viral segment abreviations in a given string.
 
     Precondition:
-    The viral segments must be writen as sgmt01_segmt02 in order to be found.
+    The viral segments must be writen as sgmt01_segmt02 in order to be
+    found.
     """
-    segments_to_regex = ''
+    segments_to_regex = ""
     for viral_segment01 in viral_segments:
         for viral_segment02 in viral_segments:
-            segments_to_regex = segments_to_regex + f'{viral_segment01}_{viral_segment02}|'
+            segments_to_regex = (
+                segments_to_regex + f"{viral_segment01}_{viral_segment02}|"
+            )
     compiled_regex = re.compile(segments_to_regex[:-1])
     return compiled_regex
 
+
 def read_arrays(data_directory, viral_segments):
     """
-    Takes a directory containing subdirectories related to diferent replicates and returns
-    two dictionaries, one describing the paths to the arrays and another containing the 
-    arrays for each segment combination.
+    Takes a directory containing subdirectories related to diferent
+    replicates and returns two dictionaries, one describing the paths to
+    the arrays and another containing the arrays for each segment
+    combination.
 
     Input:
         
@@ -50,16 +56,19 @@ def read_arrays(data_directory, viral_segments):
     d_repDir2Combinations = {}
     d_combination2Array = {}
     segments_to_regex = regex_from_segments(viral_segments)
-    replicateDirs = [entry for entry in os.listdir(data_directory) 
-                     if os.path.isdir(f'{data_directory}/{entry}')]
-    
+    replicateDirs = [
+        entry
+        for entry in os.listdir(data_directory)
+        if os.path.isdir(f"{data_directory}/{entry}")
+    ]
+
     for repDir in replicateDirs:
-        allCombinations = os.listdir(f'{data_directory}/{repDir}')
+        allCombinations = os.listdir(f"{data_directory}/{repDir}")
         d_repDir2Combinations[repDir] = allCombinations
 
         for combination in allCombinations:
-            uniqueID = f'{segments_to_regex.search(combination).group()}'
-            array = np.load(f'{data_directory}/{repDir}/{combination}')
+            uniqueID = f"{segments_to_regex.search(combination).group()}"
+            array = np.load(f"{data_directory}/{repDir}/{combination}")
             if uniqueID in d_combination2Array:
                 d_combination2Array[uniqueID].append(array)
             else:
@@ -70,7 +79,8 @@ def read_arrays(data_directory, viral_segments):
 
 def calculate_variances(d_arrays):
     """
-    Takes a dictionary with numpy arrays and calculates the variance for each unique entry.
+    Takes a dictionary with numpy arrays and calculates the variance for
+    each unique entry.
 
     Input:
         d_arrays -- {'uniqueName' : [np.arrays]}
@@ -82,14 +92,14 @@ def calculate_variances(d_arrays):
     d_comb2variance = {}
     for combination, l_countTable in d_arrays.items():
         d_comb2variance[combination] = np.var(l_countTable, axis=0)
-    
+
     return d_comb2variance
 
 
 def save_heatmaps(variances, output_dir):
     """
-    Takes a dictionary of np.arrays and plots heatmaps for the np.arrays and saves them
-    to the output_dir
+    Takes a dictionary of np.arrays and plots heatmaps for the np.arrays
+    and saves them to the output_dir
 
     Input:
 
@@ -97,19 +107,20 @@ def save_heatmaps(variances, output_dir):
     """
     # Retrieves the key (comb) and the value (variance_array)
     for comb, variance_array in variances.items():
-        plot_heatmap(variance_array, f'{output_dir}/{comb}_hist')
+        plot_heatmap(variance_array, f"{output_dir}/{comb}_heat")
 
 
 def check_characteristics(variances, output_dir):
     d_comb2characteristics = {}
 
     for comb, variance_array in variances.items():
-        k2, d_comb2characteristics[comb] = stats.normaltest(variance_array.flatten())      
+        k2, d_comb2characteristics[comb] = stats.normaltest(
+            variance_array.flatten()
+        )
 
-    return d_comb2p  
+    return d_comb2p
 
 
 def save_histograms(variances, output_dir):
     for comb, variance_array in variances.items():
-        plot_histogram(variance_array, f'{output_dir}/{comb}_hist')
-
+        plot_histogram(variance_array, f"{output_dir}/{comb}_hist")
