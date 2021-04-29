4a4525a266deaded86a1bda0c93557d90568b2ef
Gabriel Lovate <gabriel_lovate@hotmail.com>
2021-04-29 22:09:59 +0200

    Plot some read-count arrays for presentation

--- driver_script.py
+++ driver_script.py
@@ -3,6 +3,7 @@
 import variance_by_element
 
 
+# Some scripts to analyse the variances across arrays:
 # %%
 # Define file paths and viral features
 DIRECTORY = "/data/dessertlocal/projects/gl_iav-splash_freiburg"
@@ -30,4 +31,36 @@ wt_d_comb2characteristics = variance_by_element.save_characteristics(
 )
 # print(wt_d_comb2characteristics)
 
+
+# Some scripts for direclty ploting the read-count arrays
 # %%
+# Define file paths and viral features
+DIRECTORY = "/data/dessertlocal/projects/gl_iav-splash_freiburg"
+INPUT = f"{DIRECTORY}/data/arrays"
+RESULT = f"{DIRECTORY}/results/202104/20210429"
+iav_segments = ["PB2", "PB1", "PA", "HA", "NP", "NA", "M", "NS"]
+
+
+# %%
+# Read the arrays from file and put them into dictionaries
+wt_d_repDir2Combinations, wt_d_combination2Array = variance_by_element.read_arrays(
+    INPUT, iav_segments
+)
+
+# %%
+# Take a look at the dictionary
+print(wt_d_combination2Array)
+
+
+# %%
+# Extract just intercations between segments PB1 and PB2
+l_PB1_PB2 = wt_d_combination2Array["PB1_PB2"]
+print(l_PB1_PB2)
+
+
+# %%
+# Plot histograms for the three arrays of PB1 and PB2
+i = 1
+for array in l_PB1_PB2:
+    variance_by_element.plot_heatmap(array, f"{RESULT}/PB1_PB2_0{i}")
+    i += 1
