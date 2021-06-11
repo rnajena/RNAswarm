861fa1d6b69a2332e639e19bcb00629566887092
Gabriel Lovate <gabriel_lovate@hotmail.com>
2021-06-11 14:08:24 +0200

    Fix heatmap plotting

--- variance_by_element.py
+++ variance_by_element.py
@@ -11,6 +11,7 @@ def plot_heatmap(array, output_dir, filename):
     heatmap = sns.heatmap(array)
     plt.figure()
     heatmap.figure.savefig(f"{output_dir}/{filename}")
+    plt.close("all")
 
 
 def plot_histogram(array, output_dir):
