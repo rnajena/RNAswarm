37b7defe4d2ea5482fa92fa79a840553207aa084
Gabriel Lovate <gabriel_lovate@hotmail.com>
2021-06-23 13:16:02 +0200

    Fix bugs and add function to extract interactions

--- peak_finding.py
+++ peak_finding.py
@@ -1,4 +1,5 @@
 import numpy as np
+from operator import itemgetter
 
 
 def std_deviation_filter(arr):
@@ -29,8 +30,17 @@ def arbitrary_filter(arr, threshold):
 
 
 def extract_coordinates(binarry_array):
-    """
-    
+    """ Return a list of lists containing coordinates for True values on a np.array, 
+    clustering those if they are neighbouring values.
+
+    Parameters
+    ----------
+    binarry_array: np.aray with binary values (True or False)
+
+    Returns
+    -------
+    res: coordinate_list
+        coordinates in the form of a list of tuples grouped if values are neighbouring
     """
     # Iterating through array and clustering together neighboring True values on the
     # same line
@@ -51,18 +61,45 @@ def extract_coordinates(binarry_array):
             for cluster in coordinate_list:
                 for coordinate in cluster:
                     if (i - 1, j) == coordinate:
-                        coordinate_list.remove([(i, j)])
+                        for cluster in coordinate_list:
+                            if (i, j) in cluster:
+                                cluster.remove((i, j))
                         cluster.append((i, j))
+            if [] in coordinate_list:
+                coordinate_list.remove([])
 
     return coordinate_list
 
 
 def extract_regions(coordinate_list):
-    """
+    """ Return a list of dictionaries containing the coordinates for a rectangle that
+    contains the interactions of a given cluster.
+
+    Parameters
+    ----------
+    coordinate_list: coordinates in the form of a list of tuples grouped if values are
+    neighbouring
+
+    Returns
+    -------
+    res: regions_list
+        list of dictionaries containing the coordinates for the four extremities of the
+        triangle.
     """
     regions_dict = {}
     for region_id in range(len(coordinate_list)):
+        i_sorted_coord_l = sorted(coordinate_list[region_id], key=itemgetter(0))
+        j_sorted_coord_l = sorted(coordinate_list[region_id], key=itemgetter(1))
+        height = i_sorted_coord_l[-1][0] - i_sorted_coord_l[0][0]
+        width = j_sorted_coord_l[-1][1] - j_sorted_coord_l[0][1]
+        if height == 0:
+            height = 1
+        if width == 0:
+            width = 1
         regions_dict[region_id] = {
-            "start": coordinate_list[region_id][0],
-            "end": coordinate_list[region_id][-1],
+            "coordinate": (i_sorted_coord_l[0][0], j_sorted_coord_l[0][1]),
+            "height": height,
+            "width": width,
         }
+
+    return regions_dict
