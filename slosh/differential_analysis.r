ecc72e3ecfe0af713ccc7bd3a57c5cb5fd4963d1
Gabriel Lovate <39889038+gabriellovate@users.noreply.github.com>
2021-09-29 15:20:16 +0200

    Implemente differential analysis

--- slosh/differential_analysis.r
+++ slosh/differential_analysis.r
@@ -1,25 +1,31 @@
 #!/usr/bin/env Rscript
-library(readr)
-library(dplyr)
-library(tximport) # I didn't manage to install it on the working conda environment, might be good to check what is going on there
-library(tximportData)
+#library("readr")
+#library("dplyr")
+#library("tximport")
+#library("tximportData")
+library("DESeq2")
 
 # Import the data for NA_NP
-NA_NP <- read_csv('../results/202108/20210824/NA_NP_interactions.csv')
+NA_NP_counts <- read.csv('results/202109/20210930/NA_NP_interactions.csv', header = FALSE, row.names=1)
+NA_NP_samples <- c("wt01", "wt02", "wt03")
+NA_NP_conditions <- c("WT","WT","WT")
+
+# Hack to make the dataframe looks like it should look to be uses as countData DEseq2 input
+NA_NP_counts <- NA_NP_counts[,2:4]
+colnames(NA_NP_counts) <- NA_NP_samples
+
+# Generate a colData dataframe for DEseq2
+NA_NP_col <- data.frame(condition = c("WT","WT","WT"))
+row.names(NA_NP_col) <- NA_NP_samples
 
 # Run DEseq2 on it
+NA_NP_dds <- DESeqDataSetFromMatrix(countData = NA_NP_counts,
+                              colData = NA_NP_col,
+                              design= ~ 1)
 
-# Generate circus plots
+NA_NP_dds <- DESeq(NA_NP_dds)
+resultsNames(NA_NP_dds) # lists the coefficients
+NA_NP_res <- results(NA_NP_dds, name="Wt NA_NP")
 
+# # Generate circus plots
 
-
-
-
-NA_NP_dds <- DESeqDataSetFromMatrix(countData = cts,
-                              colData = coldata,
-                              design= ~ batch + condition)
-dds <- DESeq(dds)
-resultsNames(dds) # lists the coefficients
-res <- results(dds, name="condition_trt_vs_untrt")
-# or to shrink log fold changes association with condition:
-res <- lfcShrink(dds, coef="condition_trt_vs_untrt", type="apeglm")
