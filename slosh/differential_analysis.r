5f996b691228e5f0666aa337fe5fb2d56a52fd2c
Gabriel Lovate <39889038+gabriellovate@users.noreply.github.com>
2021-10-26 11:11:15 +0200

    Run DEG-like analysis on fake data

--- slosh/differential_analysis.r
+++ slosh/differential_analysis.r
@@ -1,19 +1,20 @@
 library("DESeq2")
 
 # Import the data for test
-input_filepath <- "/home/ru27wav/Projects/gl_iav-splash_freiburg/results/test/wt_mut_interactions.csv"
+input_filepath <- "/home/gabriellovate/Bierinf/Projects/gl_iav-splash_freiburg/results/IAV_wt_vs_mut/wt_mut_interactions.csv"
 
-output_filepath = /home/ru27wav/Projects/gl_iav-splash_freiburg/results/test/deseq2_output.csv
+output_filepath = "/home/gabriellovate/Bierinf/Projects/gl_iav-splash_freiburg/results/IAV_wt_vs_mut/deseq2_output_wt_mut_interactions.csv"
 
-counts <- read.csv(input_filepath, header = FALSE, row.names=1)
-conditions <- c("wt", "mut"
+counts <- read.csv(input_filepath, header = 1, row.names=1)
+samples <- c("wt01", "wt02", "wt03", "mut01", "mut02", "mut03")
+conditions <- c("wt", "mut")
 
 # Hack to make the dataframe looks like what it should look to be used as countData DEseq2 input
-counts <- counts[,2:4]
+counts <- counts[,1:6]
 colnames(counts) <- samples
 
 # Generate a colData dataframe for DEseq2
-col <- data.frame(condition = c("WT","WT","WT"))
+col <- data.frame(conditions = c("wt", "wt","wt","mut","mut","mut"))
 row.names(col) <- samples
 
 # Run DEseq2 on it
@@ -23,7 +24,8 @@ dds <- DESeqDataSetFromMatrix(countData = counts,
 
 dds <- DESeq(dds)
 resultsNames(dds) # lists the coefficients
-res <- results(dds, name="Wt NA_NP")
+res <- results(dds)
+resOrdered <- res[order(res$pvalue),]
 
 write.csv(as.data.frame(resOrdered), 
           file=output_filepath)
