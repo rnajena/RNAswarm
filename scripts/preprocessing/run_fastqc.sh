39e01e59939c71dc58074dbb1569682e8df3e7ac
Gabriel Lencioni Lovate <39889038+gabriellovate@users.noreply.github.com>
2021-12-16 21:42:34 +0100

    fix: make script more modular

--- scripts/preprocessing/run_fastqc.sh
+++ scripts/preprocessing/run_fastqc.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+
+READS_DIR=/home/ru27wav/Projects/gl_iav-splash_freiburg/data/dadonaite_2019/reads
+OUTPUT_DIR=/home/ru27wav/Projects/gl_iav-splash_freiburg/results/dadonaite_2019
+THREADS=72
+
 # Run fastqc on a repository full of fastq files
-fastqc -t 32 -o /home/ru27wav/Projects/gl_iav-splash_freiburg/results/dadonaite_2019/fastqc/fastp\
-                /home/ru27wav/Projects/gl_iav-splash_freiburg/results/dadonaite_2019/fastp/*/*.fastq\
-            &>> /home/ru27wav/Projects/gl_iav-splash_freiburg/results/dadonaite_2019/fastqc/fastp/fastqc.log
+FASTQ_FILES=$(find $READS_DIR | grep .fastq | xargs ls)
+
+for FASTQ_FILE in $FASTQ_FILES
+do
+    fastqc -t $THREADS $FASTQ_FILE -o $OUTPUT_DIR &>> $OUTPUT_DIR/fastqc.log
+done
