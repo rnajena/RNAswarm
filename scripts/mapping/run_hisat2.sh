68ed94e13712682c4038216ee09996ba424af264
Gabriel Lencioni Lovate <39889038+gabriellovate@users.noreply.github.com>
2022-01-20 21:46:31 +0100

    draft: include more draft scripts

--- scripts/mapping/run_hisat2.sh
+++ scripts/mapping/run_hisat2.sh
@@ -7,8 +7,8 @@ READS_DIR=/home/ru27wav/Projects/gl_iav-splash_freiburg/data/dadonaite_2019/read
 mkdir -p $OUTPUT_DIR/indexes
 for file in $GENOME_DIR/*.fasta; do
     echo $file
-    hisat2-build $file $OUTPUT_DIR/indexes/$(basename $file .fasta)
+    hisat2-build $file $OUTPUT_DIR/hisat2_indexes/$(basename $file .fasta)
 done
 
 # Run hisat2 with split read mapping enabled
-hisat2 -p 64 -x $OUTPUT_DIR/indexes/pr8_dadonaite -1 $READS_DIR/pr8/SRR7350059.fastq -2 $READS_DIR/pr8/SRR9637504.fastq -S $OUTPUT_DIR/mappings/pr8/SRR7350059.sam
+hisat2 -p 64 -x $OUTPUT_DIR/indexes/pr8_dadonaite -U $READS_DIR/pr8/SRR7350059.fastq | samtools sort
