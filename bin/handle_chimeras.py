4abb7bab3d4fd04fe2476e7b75ba8208cd4ef65e
Gabriel Lencioni Lovate <39889038+gabriellovate@users.noreply.github.com>
2022-04-10 23:08:38 +0200

    fix: backport Kevin's fix for genome parsing

--- bin/handle_chimeras.py
+++ bin/handle_chimeras.py
@@ -6,14 +6,34 @@ import sys
 
 
 def parse_genome(genome_file):
+    """
+    """
     genome_dict = {}
+
+    header = ""
+    seq = ""
+
     with open(genome_file) as f:  # read genome file
         for line in f:  # parse genome file
             if line.startswith(">"):  # parse header
-                name = line.strip().split(" ")[0][1:]  # remove '>' and ' '
-                genome_dict[name] = ""  # create new entry in genome dict
-            elif line != "\n":  # parse sequence and skips newlines
-                genome_dict[name] = line.strip()  # append sequence to entry
+            #* if there is a header already, we store the current sequence
+            #* to this header.
+                if header:
+                    genome_dict[header] = seq
+                    #* then we flush the sequence
+                    seq = ""
+                #* and parse the new header
+                header = line.strip().split(" ")[0][1:]  # remove '>' and ' '
+            #elif line != "\n":  # parse sequence and skips newlines
+            else:
+                #* if no header is detected, we append the new line
+                #* to the current sequence
+                seq += line.strip()
+                #genome_dict[name] = line.strip()  # append sequence to entry
+        #* after the last line, we have to store
+        #* the last sequence as well. Since no new line with ">" occurs, we
+        #* do this manually
+        genome_dict[header] = seq
     return genome_dict
 
 
