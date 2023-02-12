79e4ab1995e539cdc8c1da5f7c3f248331ae4a86
Gabriel Lencioni Lovate <39889038+gabriellovate@users.noreply.github.com>
2023-02-12 22:05:47 +0100

    feat: run deseq2 analysis

--- bin/run_DESeq2.r
+++ bin/run_DESeq2.r
@@ -3,20 +3,21 @@
 library("DESeq2")
 
 # Import the data for test
-input_filepath <- "/home/gabriellovate/Bierinf/Projects/gl_iav-splash_freiburg/results/IAV_wt_vs_mut/wt_mut_interactions.csv"
+input_filepath <- "/home/ru27wav/Projects/gl_iav-splash_freiburg/results/schwemmle_group/8xHA_update0122_CJ_cleaned_counttable.csv"
 
-output_filepath = "/home/gabriellovate/Bierinf/Projects/gl_iav-splash_freiburg/results/IAV_wt_vs_mut/deseq2_output_wt_mut_interactions.csv"
+output_filepath = "/home/ru27wav/Projects/gl_iav-splash_freiburg/results/schwemmle_group/8xHA_update0122_CJ_cleaned_counttable_deseq2.csv"
 
 counts <- read.csv(input_filepath, header = 1, row.names=1)
-samples <- c("wt01", "wt02", "wt03", "mut01", "mut02", "mut03")
-conditions <- c("wt", "mut")
+samples <- c("SC35M_WTWT_repli01_0120_trimmed","SC35M_WTWT_repli01_1021_trimmed","SC35M_WTWT_repli02_1120_trimmed","SC35M_WTWT_repli03_1120_trimmed","8-wt_S8_R1_001_trimmed","7wtintakt_S7_R1_001_trimmed","H-4_S14_R1_001_trimmed","SC35M_8xHA_repli01_1021_trimmed","SC35M_8xHA_repli02_1021_trimmed","SC35M_8xHA_repli03_1021_trimmed")
+
+conditions <- c("wt", "8xha")
 
 # Hack to make the dataframe looks like what it should look to be used as countData DEseq2 input
-counts <- counts[,1:6]
+# counts <- counts[,1:6]
 colnames(counts) <- samples
 
 # Generate a colData dataframe for DEseq2
-col <- data.frame(conditions = c("wt", "wt","wt","mut","mut","mut"))
+col <- data.frame(conditions = c("wt","wt","wt","wt","wt","wt","wt","8xha","8xha","8xha"))
 row.names(col) <- samples
 
 # Run DEseq2 on it
