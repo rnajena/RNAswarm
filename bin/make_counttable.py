3bc1f3c1594288ff5ac90a0c84657db4fc2561eb
Gabriel Lencioni Lovate <39889038+gabriellovate@users.noreply.github.com>
2023-02-20 13:27:22 +0100

    chore: refactor code style

--- bin/make_counttable.py
+++ bin/make_counttable.py
@@ -23,7 +23,14 @@ import pandas as pd
 import trns_handler as th
 
 
-def fill_count_table(interaction, count_table, annotation_table_dict, trns_file, use_peaks=False, window_size=20):
+def fill_count_table(
+    interaction,
+    count_table,
+    annotation_table_dict,
+    trns_file,
+    use_peaks=False,
+    window_size=20,
+):
     """
     Fills the count table with the number of interactions between the given
     regions.
@@ -55,41 +62,46 @@ def fill_count_table(interaction, count_table, annotation_table_dict, trns_file,
     # Loop through the annotation table dictionary
     for row in annotation_table_dict.values():
         # Get the interaction id
-        interaction_id = int(row['id'])
+        interaction_id = int(row["id"])
         # Initialize start and end positions for segments
         start01, end01, start02, end02 = 0, 0, 0, 0
         # Calculate start and end positions for segments
         if use_peaks:
-            start01 = row['segment01_peak'] - window_size
-            end01 = row['segment01_peak'] + window_size
-            start02 = row['segment02_peak'] - window_size
-            end02 = row['segment02_peak'] + window_size
+            start01 = row["segment01_peak"] - window_size
+            end01 = row["segment01_peak"] + window_size
+            start02 = row["segment02_peak"] - window_size
+            end02 = row["segment02_peak"] + window_size
         else:
-            start01, end01, start02, end02 = row['start01'], row['end01'], row['start02'], row['end02']
+            start01, end01, start02, end02 = (
+                row["start01"],
+                row["end01"],
+                row["start02"],
+                row["end02"],
+            )
         if row["segment01"] == interaction[0] and row["segment02"] == interaction[3]:
             if (
-                start01 <= interaction[1] <= end01 or
-                start01 <= interaction[2] <= end01 or
-                interaction[1] <= start01 <= interaction[2] or
-                interaction[1] <= end01 <= interaction[2]
+                start01 <= interaction[1] <= end01
+                or start01 <= interaction[2] <= end01
+                or interaction[1] <= start01 <= interaction[2]
+                or interaction[1] <= end01 <= interaction[2]
             ) and (
-                start02 <= interaction[4] <= end02 or
-                start02 <= interaction[5] <= end02 or
-                interaction[4] <= start02 <= interaction[5] or
-                interaction[4] <= end02 <= interaction[5]
+                start02 <= interaction[4] <= end02
+                or start02 <= interaction[5] <= end02
+                or interaction[4] <= start02 <= interaction[5]
+                or interaction[4] <= end02 <= interaction[5]
             ):
                 count_table[trns_file][interaction_id] += 1
         if row["segment02"] == interaction[0] and row["segment01"] == interaction[3]:
             if (
-                start01 <= interaction[4] <= end01 or
-                start01 <= interaction[5] <= end01 or
-                interaction[4] <= start01 <= interaction[5] or
-                interaction[4] <= end01 <= interaction[5]
+                start01 <= interaction[4] <= end01
+                or start01 <= interaction[5] <= end01
+                or interaction[4] <= start01 <= interaction[5]
+                or interaction[4] <= end01 <= interaction[5]
             ) and (
-                start02 <= interaction[1] <= end02 or
-                start02 <= interaction[2] <= end02 or
-                interaction[1] <= start02 <= interaction[2] or
-                interaction[1] <= end02 <= interaction[2]
+                start02 <= interaction[1] <= end02
+                or start02 <= interaction[2] <= end02
+                or interaction[1] <= start02 <= interaction[2]
+                or interaction[1] <= end02 <= interaction[2]
             ):
                 count_table[trns_file][interaction_id] += 1
 
@@ -116,7 +128,7 @@ def make_count_table(annotation_table, trns_files, use_peaks=False):
     for trns_file in trns_files:
         count_table[trns_file] = {}
         for row in annotation_table_dict.values():
-            count_table[trns_file][int(row['id'])] = 0
+            count_table[trns_file][int(row["id"])] = 0
     for trns_file in trns_files:
         with open(trns_file) as input_stream:
             for line in input_stream:
@@ -128,7 +140,11 @@ def make_count_table(annotation_table, trns_files, use_peaks=False):
                 ) + th.__extract_start_stop_segemehl(secondRead)
                 interaction = th.__check_interaction(currentRow)
                 fill_count_table(
-                    interaction, count_table, annotation_table_dict, trns_file, use_peaks=use_peaks
+                    interaction,
+                    count_table,
+                    annotation_table_dict,
+                    trns_file,
+                    use_peaks=use_peaks,
                 )
     return count_table
 
@@ -140,7 +156,9 @@ def main():
     # Read trns files
     trns_files = args["<input_file>"]
     # Create count table
-    count_table = make_count_table(annotation_table, trns_files, use_peaks=args["--use_peaks"])
+    count_table = make_count_table(
+        annotation_table, trns_files, use_peaks=args["--use_peaks"]
+    )
     # Transform count table to pandas.DataFrame
     count_table = pd.DataFrame(count_table)
     # Write count table to file
