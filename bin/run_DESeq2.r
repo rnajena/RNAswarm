1ccc5a35dc2100ee5efbf4febd74290c2814b930
Gabriel Lencioni Lovate <39889038+gabriellovate@users.noreply.github.com>
2023-02-21 14:36:32 +0100

    feature: add cli arguments to the R script

--- bin/run_DESeq2.r
+++ bin/run_DESeq2.r
@@ -1,64 +1,30 @@
 #!/usr/bin/env Rscript
 
+'Run DESeq2 on two count tables and output a tsv file with the results
+
+Usage:
+  run_DESeq2.r --count_table1 <count_table1> --alias1 <alias1> --count_table2 <count_table2> --alias2 <alias2> --output_file <output_file>
+  run_DESeq2.r (-h | --help)
+
+Options:
+    -h --help                       Show this screen.
+    --count_table1 <count_table1>   Path to the count table 1
+    --alias1 <alias1>               Alias for the first count table
+    --count_table2 <count_table2>   Path to the count table 2
+    --alias2 <alias2>               Alias for the second count table
+    --output_file <output_file>     Path to the output file
+
+' -> doc
+
+library(docopt)
+arguments <- docopt(doc)
+
 # This script runs DESeq2 on two count tables and outputs a tsv file with the results
 library("DESeq2")
 
-
-# Define command line options
-options = list(
-    count_table1 = "",
-    alias1 = "",
-    count_table2 = "",
-    alias2 = "",
-    output_file = ""
-)
-
-
-# Parse command line options
-while(length(commandArgs(TRUE)) > 0) {
-    arg = commandArgs(TRUE)[1]
-    if(arg == "--count_table1") {
-        options$count_table1 = commandArgs(TRUE)[2]
-        commandArgs(TRUE) = commandArgs(TRUE)[3:length(commandArgs(TRUE))]
-    } else if(arg == "--alias1") {
-        options$alias1 = commandArgs(TRUE)[2]
-        commandArgs(TRUE) = commandArgs(TRUE)[3:length(commandArgs(TRUE))]
-    } else if(arg == "--count_table2") {
-        options$count_table2 = commandArgs(TRUE)[2]
-        commandArgs(TRUE) = commandArgs(TRUE)[3:length(commandArgs(TRUE))]
-    } else if(arg == "--alias2") {
-        options$alias2 = commandArgs(TRUE)[2]
-        commandArgs(TRUE) = commandArgs(TRUE)[3:length(commandArgs(TRUE))]
-    } else if(arg == "--output_file") {
-        options$output_file = commandArgs(TRUE)[2]
-        commandArgs(TRUE) = commandArgs(TRUE)[3:length(commandArgs(TRUE))]
-    } else {
-        stop("Unknown argument: ", arg)
-    }
-}
-
-
-# Check if required options are provided
-if(options$count_table1 == "") {
-    stop("Please provide a count table 1 with --count_table1")
-}
-if(options$alias1 == "") {
-    stop("Please provide an alias for count table 1 with --alias1")
-}
-if(options$count_table2 == "") {
-    stop("Please provide a count table 2 with --count_table2")
-}
-if(options$alias2 == "") {
-    stop("Please provide an alias for count table 2 with --alias2")
-}
-if(options$output_file == "") {
-    stop("Please provide an output file with --output_file")
-}
-
-
 # Import the data for test
-counts1 <- read.csv(options$count_table1, header = 1, row.names=1)
-counts2 <- read.csv(options$count_table2, header = 1, row.names=1)
+counts1 <- read.csv(arguments$count_table1, header = 1, row.names=1)
+counts2 <- read.csv(arguments$count_table2, header = 1, row.names=1)
 
 
 # samples vector is the header of the count table but only the base name of the sample
@@ -67,10 +33,11 @@ samples2 <- colnames(counts2)
 
 
 # Generate a colData dataframe for DEseq2 and merge the two count tables
-col <- data.frame(conditions = c(rep(options$alias1, length(samples1)), rep(options$alias2, length(samples2))))
+col <- data.frame(conditions = c(rep(arguments$alias1, length(samples1)), rep(arguments$alias2, length(samples2))))
 row.names(col) <- c(samples1, samples2)
 counts <- merge(counts1, counts2, by="row.names")
 counts <- counts[,-1] # remove the first column which is the row names
+counts <- as.matrix(counts) # convert to matrix
 
 
 # Run DEseq2 on it
@@ -82,10 +49,8 @@ resultsNames(dds) # lists the coefficients
 res <- results(dds)
 resOrdered <- res[order(res$pvalue),]
 
-# write.csv(as.data.frame(resOrdered), 
-#           file=output_filepath)
 
 # Write the results to a tsv file
 write.table(as.data.frame(resOrdered), 
-            file=options$output_file, 
+            file=arguments$output_file, 
             sep="\t")
