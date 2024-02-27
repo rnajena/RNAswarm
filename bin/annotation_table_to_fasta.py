989b1e9285d2988be26c7617c33898754f8b97e9
Gabriel Lencioni Lovate <39889038+gabriellovate@users.noreply.github.com>
2024-02-27 13:56:31 +0100

    chore: refactor script to use argparse and improve code structure

--- bin/annotation_table_to_fasta.py
+++ bin/annotation_table_to_fasta.py
@@ -1,46 +1,81 @@
 #!/usr/bin/env python3
 
-"""annotation table to fasta.
-
-Usage:
-    annotation_table_to_fasta.py -h | --help
-    annotation_table_to_fasta.py -a <annotation_table> -g <genome_file>... -o <output_file>
-
-Options:
-    -h --help                         Show this screen.
-    -a --annotation_table=<annotation_table>     The annotation table.
-    -g --genome_file=<genome_file>     The genome file. Can be specified multiple times.
-    -o --output_file=<output_file>     The output file.
-"""
-from docopt import docopt
+import argparse
 import deduplicate_annotations as da
-from Bio import SeqIO
+import helper as hp
 import pandas as pd
+from pathlib import Path
 
 
-def annotation_to_sequence(annotation, genome_file):
-    """Extracts the sequence of the annotation from the genome file.
+def annotation_to_sequence(annotation, genome_dict, extension=0):
+    """Extracts the sequences of the annotation from the genome file.
 
     Args:
-        
+        annotation (pandas.Series): The annotation.
+        genomes_dict (dict): A dictionary containing the genome.
+        extension (int): The extension to add to the start and end of the annotation.
     Returns:
-        sequence (str): The sequence(s) of the annotation.
+        sequences (list): The sequences of the annotation.
     """
-    genome = SeqIO.read(genome_file, "fasta")
-    sequence = genome.seq[annotation['start']-1:annotation['end']]
-    if annotation['strand'] == '-':
-        sequence = sequence.reverse_complement()
-    return sequence
+    sequences = [
+        genome_dict[annotation["segment01"]][
+            annotation["start01"] + extension : annotation["end01"] + extension
+        ],
+        genome_dict[annotation["segment02"]][
+            annotation["start02"] + extension : annotation["end02"] + extension
+        ],
+    ]
+    return sequences
+
+
+def write_fasta(sequences, output_file):
+    """Writes the sequences to a fasta file.
+
+    Args:
+        sequences (dict): The sequences to write.
+        output_file (str): The output file.
+    """
+    with open(output_file, "w") as file:
+        for index, sequence in sequences.items():
+            file.write(f">{index}_seq01\n")
+            file.write(f"{sequence[0]}\n")
+            file.write(f">{index}_seq02\n")
+            file.write(f"{sequence[1]}\n")
 
 
 def main():
-    args = docopt(__doc__)
-    annotation_table = args['--annotation_table']
-    genome_file = args['--genome_file']
-    output_file = args['--output_file']
-    annotation_table_df = da.parse_annotation_table(annotation_table)
-    annotation_table_df['sequence'] = annotation_table_df.apply(lambda row: annotation_to_sequence(row, genome_file), axis=1)
-    annotation_table_df
+    parser = argparse.ArgumentParser(description="annotation table to fasta.")
+    parser.add_argument("-g", "--genome_file", help="The genome file.")
+    parser.add_argument("-a", "--annotation_table", help="The annotation table.")
+    parser.add_argument("-o", "--output_folder", help="The output folder.")
+    args = parser.parse_args()
+
+    annotation_table_df = da.parse_annotation_table(args.annotation_table)
+    genome_dict = hp.parse_fasta(args.genome_file)
+
+
+    sequences = {}
+    # iterate over each line of the annotation_table_df
+    for index, row in annotation_table_df.iterrows():
+        # extract the sequences of the annotation from the genome file
+        interaction = annotation_to_sequence(row, genome_dict)
+        sequences[index] = interaction
+
+
+    sequences_extended = {}
+    # iterate over each line of the annotation_table_df
+    for index, row in annotation_table_df.iterrows():
+        # extract the sequences of the annotation from the genome file
+        interaction = annotation_to_sequence(row, genome_dict, extension=25)
+        sequences_extended[index] = interaction
+
+
+    # write the sequences to a fasta file
+    output_file = Path(args.output_folder) / Path(f"{Path(args.genome_file).stem}_annotations.fasta")
+    output_file_extended = Path(args.output_folder) / Path(f"{Path(args.genome_file).stem}_annotations_extended.fasta")
+    write_fasta(sequences, output_file)
+    write_fasta(sequences_extended, output_file_extended)
+
 
 if __name__ == "__main__":
     main()
