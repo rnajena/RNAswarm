9178c8abec0f523b85467cb503a0376fecf12b97
Gabriel Lencioni Lovate <39889038+gabriellovate@users.noreply.github.com>
2025-09-05 00:09:57 +0200

    fix: update command-line options and logic for rounding in array normalization

--- bin/normalize_arrays.py
+++ bin/normalize_arrays.py
@@ -6,7 +6,7 @@
     Normalize multiple numpy arrays.
 
 Usage:
-    normalize_arrays.py -d <sample_arrays> -g <genome> -o <sample_arrays_normalized> [--max_value=<max_value> --mode=<mode> --round]
+    normalize_arrays.py -d <sample_arrays> -g <genome> -o <sample_arrays_normalized> [--max_value=<max_value> --mode=<mode> --do_not_round]
     normalize_arrays.py -h | --help
 
 Options:
@@ -30,17 +30,19 @@ def main():
     output_file = args["-o"]
     max_value = int(args["--max_value"])
     mode = args["--mode"]
-    round = args["--round"]
-    if round in ["True", "true", "1"]:
-        round = True
-    else:
+    do_not_round = args["--do_not_round"]
+    if do_not_round in ["True", "true", "1"]:
         round = False
+    else:
+        round = True
 
     genome_dict = hp.parse_fasta(genome_file)
     combination_arrays = hp.make_combination_array(genome_dict, intra_only=False)
+    combination_arrays_normalized = combination_arrays.copy()
     ah.import_combination_arrays(combination_arrays, sample_arrays)
-    normalized_arrays = ah.normalize_array(combination_arrays, max_value=max_value, mode=mode, round=round)
-    ah.save_combination_arrays(normalized_arrays, output_file)
+    for combination, array in combination_arrays.items():
+        combination_arrays_normalized[combination] = ah.normalize_array(array, max_value=max_value, mode=mode, round=round)
+    ah.save_combination_arrays(combination_arrays_normalized, output_file)
 
 if __name__ == "__main__":
     main()
